c894def076cdb46bd96bd63d42639988aca4f4a7
Xadman <90847817+Xadman@users.noreply.github.com>
2022-03-03 09:47:39 -0600

    Finished sound and vis affects

--- Assets/Scripts/MoveLeft.cs
+++ Assets/Scripts/MoveLeft.cs
@@ -4,24 +4,24 @@ using UnityEngine;
 
 public class MoveLeft : MonoBehaviour
 {
-    public float speed = 20;
-    private PlayerController playerControllerScript;
-    private float leftBound = -15;
+    public float speed = 20; // created a variable called speed
+    private PlayerController playerControllerScript; // called player controller script
+    private float leftBound = -15; // set boundry limit
 
     // Start is called before the first frame update
     void Start()
     {
-        playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
+        playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>(); /// player will restart where it is placed in scene
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (playerControllerScript.gameOver == false)
+        if (playerControllerScript.gameOver == false) 
         {
-            transform.Translate(Vector3.left * Time.deltaTime * speed);
+            transform.Translate(Vector3.left * Time.deltaTime * speed); // lets us set speed of player
         }
-        if (transform.position.x < leftBound && gameObject.CompareTag("Obstacle"))
+        if (transform.position.x < leftBound && gameObject.CompareTag("Obstacle")) // once obstacle passes player and gets to edge of scene it is destroyed
         {
             Destroy(gameObject);
         }
